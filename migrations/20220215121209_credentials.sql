95b768859a348dec9a41c300460082bb4b26c6a6
sope shen <shenshouer51@gmail.com>
2022-02-23 10:31:50 +0800

    change github actions

--- migrations/20220215121209_credentials.sql
+++
@@ -1,7 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
-CREATE TABLE IF NOT EXISTS credentials (
-   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-   username VARCHAR(50) NOT NULL unique,
-   password TEXT NOT NULL
-);
